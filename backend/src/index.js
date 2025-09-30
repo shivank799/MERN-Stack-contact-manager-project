fbe544185e19cc72ad18d16056c83fde35d9e5d2
Shivank Upmayu <shivanks799@gmail.com>
2025-09-30 11:34:20 +0530

    added docker-compose.yml

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,12 @@ import mongoose from 'mongoose';
 import { createApp } from './app.js';
 import { config } from './config.js';
 
+// Determine MongoDB URI based on the environment
+const mongoUri = process.env.MONGO_URI || config.mongoUri;
+
 const app = createApp();
 
-mongoose.connect(config.mongoUri).then(() => {
+mongoose.connect(mongoUri).then(() => {
   app.listen(config.port, () => {
     console.log(`API listening on http://0.0.0.0:${config.port}`);
   });
